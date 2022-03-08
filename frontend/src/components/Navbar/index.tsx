7ea956678bcc1ed0686f032bdadcf543a5c1cfc1
Joi Macedo <joi.macedo@hotmail.com>
2022-03-08 11:13:34 -0300

    form

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,24 +1,22 @@
-import{ReactComponent as GithubIcon} from 'assets/img/github.svg'
-import './styles.css'
+import { ReactComponent as GithubIcon } from "assets/img/github.svg";
+import "./styles.css";
 
-function Navbar(){
-    return(
-        <header>
-        <nav className="container">
-          <div className="dsmovie-nav-content">
-            <h1>
-              DSMovie
-            </h1>
-            <a href="https://github.com/Joistick45">
-              <div className="dsmovie-contact-container">
-                <GithubIcon/>
-                <p className="dsmovie-contact-link">/devsuperior</p>
-              </div>
-            </a>
-          </div>
-        </nav>
-      </header>
-    )
+function Navbar() {
+  return (
+    <header>
+      <nav className="container">
+        <div className="dsmovie-nav-content">
+          <h1>DSMovie</h1>
+          <a href="https://github.com/Joistick45">
+            <div className="dsmovie-contact-container">
+              <GithubIcon />
+              <p className="dsmovie-contact-link">/devsuperior</p>
+            </div>
+          </a>
+        </div>
+      </nav>
+    </header>
+  );
 }
 
 export default Navbar;
--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,6 +1,4 @@
-function Listing(){
-    return(
-        <h1>Página de Listagem</h1>
-    );
+function Listing() {
+  return <h1>Página de Listagem</h1>;
 }
 export default Listing;
